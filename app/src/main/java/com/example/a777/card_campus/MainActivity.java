9616431dfe5af64076361f7cd0643a9c218703f4
YiyaCheng <cyiyacheng@163.com>
2018-04-06 10:39:56 +0800

    test

--- app/src/main/java/com/example/a777/card_campus/MainActivity.java
+++ app/src/main/java/com/example/a777/card_campus/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //000
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
