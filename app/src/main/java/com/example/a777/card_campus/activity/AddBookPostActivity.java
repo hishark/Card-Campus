0b6c5048fd758b54e84f96ae8a2ff4c6a0d83fba
YiyaCheng <cyiyacheng@163.com>
2018-06-08 13:55:49 +0800

    删除多余代码

--- app/src/main/java/com/example/a777/card_campus/activity/AddBookPostActivity.java
+++ app/src/main/java/com/example/a777/card_campus/activity/AddBookPostActivity.java
@@ -2,11 +2,8 @@ package com.example.a777.card_campus.activity;
 
 import android.Manifest;
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,34 +11,22 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.MediaStore;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.FileProvider;
-import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v7.app.AppCompatActivity;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
 import com.example.a777.card_campus.R;
-import com.example.a777.card_campus.qiniuUtils.Auth;
-import com.example.a777.card_campus.util.AnimatorUtil;
+import com.example.a777.card_campus.qiniuyun.Auth;
 import com.example.a777.card_campus.util.CurrentUserUtil;
 import com.example.a777.card_campus.util.RandomIDUtil;
 import com.example.a777.card_campus.util.ToastUtil;
 import com.qiniu.android.http.ResponseInfo;
 import com.qiniu.android.storage.UpCompletionHandler;
 import com.qiniu.android.storage.UploadManager;
-import com.zhihu.matisse.Matisse;
-import com.zhihu.matisse.MimeType;
-import com.zhihu.matisse.engine.impl.GlideEngine;
 
 import org.json.JSONObject;
 
@@ -50,10 +35,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
